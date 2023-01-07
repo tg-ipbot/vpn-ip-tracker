21aa8d9a8cd59465ed3f122dcd31ca6b94a5735f
Vladimir Petrigo <vladimir.petrigo@gmail.com>
2023-01-07 18:36:46 +0300

    app: Clean up

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 /* SPDX-License-Identifier: MIT OR Apache-2.0 */
 use log::{debug, warn};
 use network_interface::{self, NetworkInterface, NetworkInterfaceConfig};
-use std::io::Read;
+use serde::Deserialize;
 
 use utils::IfaceInfo;
 
@@ -10,10 +10,41 @@ mod utils;
 const REPORT_URL_VAR: &str = "IPREPORT_ADDR";
 const TOKEN_ENV_VAR: &str = "IPREPORT_APP_TOKEN";
 const CONFIG_JSON_FILE: &str = "config.json";
+const REPORT_CERT_FILE: &str = "cert.pem";
 
-fn main() {
+#[derive(Debug, PartialEq, Deserialize)]
+struct TrackerConfig {
+    token: String,
+    #[serde(rename(deserialize = "reportUrl"))]
+    report_url: String,
+}
+
+impl TrackerConfig {
+    fn from_env() -> Option<Self> {
+        let report_url = std::env::var(REPORT_URL_VAR);
+        let token = std::env::var(TOKEN_ENV_VAR);
+
+        if report_url.is_err() || token.is_err() {
+            return None;
+        }
+
+        let report_url = report_url.unwrap();
+        let token = token.unwrap();
+
+        Some(Self { token, report_url })
+    }
+}
+
+fn main() -> Result<(), std::io::Error> {
     env_logger::init();
     let mut stored_iface: Option<IfaceInfo> = None;
+    let config = load_config();
+
+    if config.is_none() {
+        return Err(std::io::Error::from(std::io::ErrorKind::InvalidInput));
+    }
+
+    let config = config.unwrap();
 
     loop {
         let net = NetworkInterface::show();
@@ -24,7 +55,7 @@ fn main() {
         }) {
             if let Ok(ser_iface) = IfaceInfo::try_from(iface.to_owned()) {
                 if stored_iface.is_none() || stored_iface.as_ref().unwrap() != &ser_iface {
-                    match send_report(&ser_iface) {
+                    match send_report(&ser_iface, &config) {
                         Ok(_) => {
                             debug!("Successfully report");
                             stored_iface = Some(ser_iface);
@@ -42,41 +73,29 @@ fn main() {
     }
 }
 
-fn load_token() -> Option<String> {
-    if let Ok(token) = std::env::var(TOKEN_ENV_VAR) {
-        return Some(token);
-    }
+fn load_config() -> Option<TrackerConfig> {
+    let config = std::fs::File::open(CONFIG_JSON_FILE);
 
-    match std::fs::File::open(CONFIG_JSON_FILE) {
-        Ok(mut config) => {
-            let mut buffer = String::new();
-
-            if config.read_to_string(&mut buffer).is_ok() {
-                if let Ok(value) = serde_json::from_str::<serde_json::Value>(&buffer) {
-                    if let Some(token) = value["token"].as_str() {
-                        return Some(token.into());
-                    }
-                }
-            }
+    if let Ok(config) = config {
+        if let Ok(config) = serde_json::from_reader(&config) {
+            return Some(config);
         }
-        _ => warn!("config.json file has not been found"),
     }
 
-    None
+    TrackerConfig::from_env()
 }
 
-fn send_report(iface: &IfaceInfo) -> reqwest::Result<()> {
+fn send_report(iface: &IfaceInfo, config: &TrackerConfig) -> reqwest::Result<()> {
     let data = iface.ip.to_string();
-    let token = load_token();
-
-    if token.is_none() {
-        panic!("Please specify token either in IPREPORT_APP_TOKEN or in the config.json file");
-    }
-
-    let headers = prepare_headers(token.unwrap());
-    let url =
-        std::env::var(REPORT_URL_VAR).expect("Please specify IPREPORT_ADDR environment variable");
-    let client = reqwest::blocking::Client::new();
+    let headers = prepare_headers(config.token.clone());
+    let url = config.report_url.clone();
+    let cert = std::fs::read(REPORT_CERT_FILE).expect("Could not find valid certificate");
+    let cert = reqwest::Certificate::from_pem(cert.as_slice()).unwrap();
+    let client = reqwest::blocking::Client::builder()
+        .tls_sni(false)
+        .add_root_certificate(cert)
+        .build()
+        .unwrap();
 
     client
         .post(url)
@@ -91,8 +110,10 @@ fn send_report(iface: &IfaceInfo) -> reqwest::Result<()> {
 
 fn prepare_headers(token: String) -> reqwest::header::HeaderMap {
     let mut header = reqwest::header::HeaderMap::new();
+    let mut token = reqwest::header::HeaderValue::from_str(&token).unwrap();
 
-    header.insert("Credential", token.parse().unwrap());
+    token.set_sensitive(true);
+    header.insert("Credential", token);
 
     header
 }
@@ -108,42 +129,46 @@ fn vpn_iface_check(iface: &NetworkInterface) -> bool {
 }
 
 #[cfg(test)]
-mod token_tests {
-    use crate::{load_token, CONFIG_JSON_FILE, TOKEN_ENV_VAR};
+mod config_tests {
+    use crate::{load_config, CONFIG_JSON_FILE, REPORT_URL_VAR, TOKEN_ENV_VAR};
 
     use std::env;
 
+    const TEST_TOKEN: &str = "some_env_token";
+    const TEST_URL: &str = "https://some_url/";
+
     #[test]
-    fn test_load_token_no_available() {
-        let token = load_token();
-        assert_eq!(token, None);
+    fn test_load_config_no_available() {
+        let config = load_config();
+        assert_eq!(config, None);
     }
 
     #[test]
-    fn test_load_token_env() {
-        const TEST_TOKEN: &str = "some_env_token";
-
-        fn setup(exp_token: &str) {
+    fn test_load_config_env() {
+        fn setup(exp_token: &str, exp_url: &str) {
             env::set_var(TOKEN_ENV_VAR, exp_token);
+            env::set_var(REPORT_URL_VAR, exp_url);
         }
 
         fn teardown() {
             env::remove_var(TOKEN_ENV_VAR);
+            env::remove_var(REPORT_URL_VAR);
         }
 
-        setup(TEST_TOKEN);
-        let token = env::var(TOKEN_ENV_VAR).expect("Failed to get token");
-        assert_eq!(token, TEST_TOKEN);
+        setup(TEST_TOKEN, TEST_URL);
+        let config = load_config().unwrap();
+
+        assert_eq!(config.token, TEST_TOKEN);
+        assert_eq!(config.report_url, TEST_URL);
         teardown();
     }
 
     #[test]
     fn test_load_token_file() {
-        const TEST_TOKEN: &str = "some_file_token";
-
-        fn setup(exp_token: &str) {
+        fn setup(exp_token: &str, exp_url: &str) {
             let data = serde_json::json![{
                 "token": exp_token,
+                "reportUrl": exp_url,
             }];
             let f = std::fs::File::create(CONFIG_JSON_FILE).unwrap();
             serde_json::ser::to_writer_pretty(f, &data).unwrap();
@@ -153,10 +178,11 @@ mod token_tests {
             std::fs::remove_file(CONFIG_JSON_FILE).expect("Failed to remove test config.json");
         }
 
-        setup(TEST_TOKEN);
-        let token = load_token();
-        assert!(token.is_some());
-        assert_eq!(token.unwrap(), TEST_TOKEN);
+        setup(TEST_TOKEN, TEST_URL);
+        let config = load_config().unwrap();
+
+        assert_eq!(config.token, TEST_TOKEN);
+        assert_eq!(config.report_url, TEST_URL);
         teardown();
     }
 }
