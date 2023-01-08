40faaaff753290cda0be06a9c81910ecbabca328
Vladimir Petrigo <vladimir.petrigo@gmail.com>
2023-01-08 21:36:09 +0300

    app: config: Add VPN Tracker configuration binary

---
+++ src/bin/config/config_linux.rs
@@ -0,0 +1,39 @@
+use std::io::{BufRead, BufReader, Error, ErrorKind, Write};
+
+use directories::BaseDirs;
+
+type Result<T> = core::result::Result<T, Error>;
+
+pub(super) fn install_service() -> Result<()> {
+    const TEMPLATE_SERVICE_FILENAME: &str = "vpn-ip-tracker.service.template";
+    let executable_path = std::env::current_exe()?;
+    let parent_dir = executable_path.parent().unwrap();
+    let template_file = std::fs::File::open(TEMPLATE_SERVICE_FILENAME)?;
+    let service_dir = BaseDirs::new()
+        .unwrap()
+        .config_dir()
+        .join("systemd")
+        .join("user");
+    std::fs::create_dir_all(&service_dir)?;
+
+    let reader = BufReader::new(template_file);
+    let mut service_file = std::fs::File::create(service_dir.join("vpn-ip-tracker.service"))?;
+
+    for line in reader.lines().flatten() {
+        let sub_start = line.find('@');
+
+        if let Some(sub_start) = sub_start {
+            let sub_end = line.rfind('@').unwrap();
+            let mut dir_str = String::from(&line[..sub_start]);
+
+            dir_str += parent_dir.to_str().unwrap();
+            dir_str += &line[sub_end + 1..];
+            writeln!(service_file, "{dir_str}")
+                .map_err(|e| Error::new(ErrorKind::InvalidData, e))?;
+        } else {
+            writeln!(service_file, "{line}").map_err(|e| Error::new(ErrorKind::InvalidData, e))?;
+        }
+    }
+
+    Ok(())
+}
---
+++ src/bin/config/main.rs
@@ -0,0 +1,35 @@
+use clap::Parser;
+use confy::ConfyError;
+
+use vpn_ip_tracker::{TrackerConfig, APP_NAME, DEFAULT_REPORT_URL};
+
+#[cfg(target_os = "linux")]
+mod config_linux;
+#[cfg(target_os = "linux")]
+use config_linux::install_service;
+
+#[cfg(target_os = "windows")]
+mod config_win;
+
+#[derive(Debug, Parser)]
+#[command(author, about = "VPN IP Tracker configuration", version)]
+struct Cli {
+    #[arg(short, long, help = "Application token")]
+    token: String,
+    #[arg(short, long, help = "URL to send reports with IP address info")]
+    report_url: Option<String>,
+}
+
+fn main() -> Result<(), ConfyError> {
+    let args = Cli::parse();
+
+    confy::store(
+        APP_NAME,
+        None,
+        TrackerConfig::new(
+            args.token,
+            args.report_url.unwrap_or_else(|| DEFAULT_REPORT_URL.into()),
+        ),
+    )?;
+    install_service().map_err(ConfyError::GeneralLoadError)
+}
